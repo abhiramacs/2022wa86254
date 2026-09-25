491503aec5567e3c5d0f1b21a3b2225fcfb417e6
abhiramacs <abhiramacs2011@gmail>
2026-09-25 12:03:00 +0530

    For LAB

---
+++ src/main/java/com/bits/devops/App.java
@@ -0,0 +1,10 @@
+package com.bits.devops;
+ 
+public class App {
+    public static void main(String[] args) {
+        System.out.println("DevOps Lab Assignment");
+        System.out.println("Name: Abhirama C S");
+        System.out.println("BITS ID: 2022wa86254");
+        System.out.println("Maven build completed successfully.");
+    }
+}
