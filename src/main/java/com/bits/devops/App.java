ca62c9d32e8806e8b2c15eaf2797ffd4414ff08d
abhiramacs <abhiramacs2011@gmail>
2026-09-25 12:47:56 +0530

    Modifiedc

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -1,10 +1,10 @@
 package com.bits.devops;
- 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("DevOps Lab Assignment");
-        System.out.println("Name: Abhirama C S");
-        System.out.println("BITS ID: 2022wa86254");
-        System.out.println("Maven build completed successfully.");
+    public class App {
+        public static void main(String[] args) {
+            System.out.println("DevOps Lab");
+            System.out.println("Student Name : Abhirama C S");
+           System.out.println("BITS ID - 2022wa86254");
+           System.out.println("Continuous Integration Demonstration");
+        }
     }
-}
+
